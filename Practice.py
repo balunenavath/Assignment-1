6debeab35b35e6824e3260b8a76d37cfb0908960
balunenavath <nenavathbalu769@gmail.com>
2025-04-13 15:23:18 +0530

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,25 @@
+## Code 1
+c= input('enter the first number: ')
+d= input('enter the second number: ')
+
+a = int(c)
+b = int(d)
+
+add = a + b
+sub = a - b
+mul = a*b
+div = a/b
+
+print('Addition: ',add)
+print('Subraction: ',sub)
+print('Multiplication: ',mul)
+print('Division: ',div)
+
+##Code 2
+
+f = input('Enter the first name: ')
+l = input('Enter the last name: ')
+n = f +' '+l
+print('Hello '+ n + '! welcome to python programme')
+
+
